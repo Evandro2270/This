c3b74fa76f971cd89fb11578f06722d61690d55f
XEON <evandro535@gmail.com>
2024-12-26 16:34:04 -0300

    Adicionar arquivos de projeto.

---
+++ This/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using VendasPodutos;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                Console.WriteLine("Entre os dados do produto: ");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Preço: ");
+                double preco = double.Parse(Console.ReadLine(), CI);
+                Console.Write("Quantidade: ");
+                int quantidade = int.Parse(Console.ReadLine());
+
+                Produto p = new Produto(nome, preco, quantidade);
+            
+                Console.WriteLine();
+                Console.WriteLine("Dados do produto: " + p);
+
+                Console.WriteLine();
+                Console.Write("Digite o numero de produto a ser adicionado no estoque: ");
+                int qte = int.Parse(Console.ReadLine());
+                p.AdicionarProdutos(qte);
+
+                Console.WriteLine();
+                Console.WriteLine("Dados do produto Atualizados: " + p);
+
+                Console.WriteLine();
+                Console.WriteLine("Digite o numero de produto a ser removido no estoque: ");
+                qte = int.Parse(Console.ReadLine());
+                p.RemoverProdutos(qte);
+                Console.WriteLine("Dados do produto Atualizados: " + p);
+
+            }
+        }
+    }
+}
+
